9ad2dacd07ac9c1eda5815f8775fccec88c17378
Zerno0o <no723918@gmail.com>
2025-01-12 02:29:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import telebot
-from bot_logic import gen_pass
+import os
+import random 
 
 # Замени 'TOKEN' на токен твоего бота
 # Этот токен ты получаешь от BotFather, чтобы бот мог работать
-bot = telebot.TeleBot("Токен")
-
-text_messages = { }
+bot = telebot.TeleBot("Token")
+    
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
-
+    
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
     bot.reply_to(message, "Привет! Как дела?")
-
+    
 @bot.message_handler(commands=['bye'])
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
 
-@bot.message_handler(commands=['password'])
-def send_password(message):
-    bot.reply_to(message, gen_pass(10))
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+    images = os.listdir('images')
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:   
+        bot.send_photo(message.chat.id, f)
 
-
+@bot.message_handler(commands=['animals'])
+def send_mem(message):
+    animals = os.listdir('animals')
+    img_name = random.choice(animals)
+    with open(f'animals/{img_name}', 'rb') as f:   
+        bot.send_photo(message.chat.id, f)
+    
+    
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-
-@bot.message_handler(commands=['ping'])
-def send_ping(message):
-    bot.reply_to(message, "Still alive and kicking!")
-
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, "Commands - hello, bye, password, ping")
-
+    
 bot.polling()
